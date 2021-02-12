33ad29bf2ecea30a2c45e01526fa23416c5a966f
BAderinto <bilqiiswunmi@gmail.com>
2021-02-12 20:01:16 +0000

    updates

---
+++ IntroductionToR.R
@@ -0,0 +1,28 @@
+# 1.0 Introduction to R programming language ----
+# Learning Outcome ----
+# Learn about R programming language and basic data structures.
+
+# 1.1 Using R ----
+
+
+# 1.2 Basic Arithmetric in R
+1
+
+2
+
+# Add numbers together
+1+2
+
+# Add strings together
+'My name is '+'Ola'
+
+# Variables
+# invalid naming style
+1student = 'Sola'
+-studentname = 'Sola'
+-studentnme = 'Sola'
+student-name <- 'Sola'
+
+studentName <- 'Sola'
+student_name = 'Sola'
+student1name <- 'Sola'
